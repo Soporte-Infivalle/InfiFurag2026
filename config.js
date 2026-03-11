3907b54c5cc182172464d986a9c4652f0b8c5258
Juan Pablo Martínez <soporte@infivalle.gov.co>
2026-03-11 11:55:59 -0500

    feat: implement encharged name

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // URL del Apps Script que recibe y guarda las respuestas en Google Sheets
   // Ver instrucciones en README.md → Paso 3
-  WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbwd8caDYEWE-jbEQCepckCejO8PAcKLxkxLfnErHZ1bw1JIkNo9vE3XzzHquGbautCGtg/exec',
+  WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbz9Xeq3B5RzeaKqjUA7bT8Ki1OZsc2SirC3eOjwSBf48TpN23x1-RkWL5Xig_xXypkK-w/exec',
 // Directorio base de datos (ruta absoluta desde la raíz del sitio)
   DATA_DIR: '/InfiFurag2026/data',
 
